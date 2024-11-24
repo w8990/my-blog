e21427719a7350618985578e80f948777ef9907c
w8990 <41657678+w8990@users.noreply.github.com>
2024-11-24 15:33:20 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-
-    </p>
-    <h3>amazon wht</h3>
-
+  <div class="personal-blog">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <h3>About Me</h3>
+    <p>{{ about }}</p>
+    <h3>Latest Posts</h3>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <a :href="post.link">{{ post.title }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'PersonalBlogIntro',
   props: {
-    msg: String
+    title: String,
+    description: String,
+    about: String,
+    posts: Array
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+p {
+  font-size: 1.2em;
+  line-height: 1.6;
+  margin: 10px 0;
+}
 h3 {
-  margin: 40px 0 0;
+  margin: 30px 0 10px;
+  color: #333;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 10px 0;
 }
 a {
   color: #42b983;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
 }
 </style>
